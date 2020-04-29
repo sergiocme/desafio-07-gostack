9106d36bd3556d6415cf4a5970a01569c1db4477
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-04-29 12:45:35 -0300

    Fix outcome value to pass on tests

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -106,10 +106,12 @@ const Dashboard: React.FC = () => {
             <tbody>
               {transactions &&
                 transactions.map(transaction => (
-                  <tr>
+                  <tr key={transaction.id}>
                     <td className="title">{transaction.title}</td>
                     <td className={transaction.type}>
-                      {transaction.formattedValue}
+                      {transaction.type === 'outcome'
+                        ? ` - ${transaction.formattedValue}`
+                        : transaction.formattedValue}
                     </td>
                     <td>{transaction.category.title}</td>
                     <td>{transaction.formattedDate}</td>
